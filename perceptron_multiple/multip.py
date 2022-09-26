43c236047c8c3290f4c306f2cd21e8db37c015e4
jlocamuz <jlocamuz@gmail.com>
2022-09-26 17:01:30 -0300

    pesos arreglados

--- perceptron_multiple/multip.py
+++ perceptron_multiple/multip.py
@@ -21,10 +21,14 @@ class Perceptron:
         # creo variables segun mi la len() de mi entrada
 
         for i in range(len(entrada)+1):
+            print(entrada)
             if i == 0:
                w0 = self.pesos[0] 
             else: 
-                vars()['x%s' % i] = entrada[i-1]
+
+
+                print(i)
+                vars()['x%s' % i] = entrada[i]
                 vars()['w%s' % i] = self.pesos[i]
     
         # realizo sumatoria 
@@ -71,6 +75,6 @@ class RedNeuronal:
 
 
 if __name__ == '__main__':
-    xor = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0]])
-    red_neuronal = RedNeuronal([[2, [0.9, 0.2, 0.5], [0.3, -0.9, -1]], [3, [0.8, 0.35, 0.1], [0.9, -0.79, 0.56], [0.9, 0.22, -0.6]], [1, [-0.22, -0.55, 0.31, -0.32]]])
+    xor = np.array([[0, 0, 0]])
+    red_neuronal = RedNeuronal([[2, [0.9, 0.7, 0.5], [0.3, -0.9, -1]], [3, [0.8, 0.35, 0.1], [-0.23, -0.79, 0.56], [0.6, -0.6, 0.22]], [1, [-0.22, -0.55, 0.31, -0.32]]])
     red_neuronal.alimentarRed(xor)
