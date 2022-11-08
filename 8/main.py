612ee298985ae8afe600d99197f73bee763d39fa
jlocamuz <jlocamuz@gmail.com>
2022-11-07 21:05:39 -0300

    automatizando procesos

--- 8/main.py
+++ 8/main.py
@@ -1,45 +1,55 @@
+import copy
+
+
+def move(tablero):
+    for indice_fila,fila in enumerate(tablero):
+        for indice_elemento,elemento in enumerate(fila):#columna 
+            if type(elemento) == str: 
+                if indice_fila - 1 >= 0:
+                    tablero[indice_fila-1][indice_elemento], tablero[indice_fila][indice_elemento] = tablero[indice_fila][indice_elemento], tablero[indice_fila - 1][indice_elemento]
+                    print('arriba')
+                if indice_elemento - 1 >= 0: 
+                    tablero[indice_fila][indice_elemento-1], tablero[indice_fila][indice_elemento] = tablero[indice_fila][indice_elemento], tablero[indice_fila][indice_elemento-1]
+                    print('izquierda')
+
+                if indice_fila + 1 <= 2:
+                    tablero[indice_fila+1][indice_elemento], tablero[indice_fila][indice_elemento] = tablero[indice_fila][indice_elemento], tablero[indice_fila + 1][indice_elemento]
+                    print('abajo')
+
+                if indice_elemento + 1 <= 2:
+                    tablero[indice_fila][indice_elemento+1], tablero[indice_fila][indice_elemento] = tablero[indice_fila][indice_elemento], tablero[indice_fila][indice_elemento+1]
+                    print('derecha')
+
 
 
 def desordenar(tablero):
     print(tablero)
     movimiento = 0
     while movimiento < 50:
-        for indice_fila,fila in enumerate(tablero):
-            for indice_elemento,elemento in enumerate(fila):#columna 
-                if type(elemento) == str: 
-                    #indice = (indice_fila, indice_elemento)
-                    if indice_fila - 1 >= 0:
-                        # se puede mover para arriba
-                        tablero[indice_fila-1][indice_elemento], tablero[indice_fila][indice_elemento] = tablero[indice_fila][indice_elemento], tablero[indice_fila - 1][indice_elemento]
-                        print('arriba')
-                        movimiento += 1
-                    if indice_elemento - 1 >= 0: 
-                        # no se puede mover para arriba pero si para la izq
-                        tablero[indice_fila][indice_elemento-1], tablero[indice_fila][indice_elemento] = tablero[indice_fila][indice_elemento], tablero[indice_fila][indice_elemento-1]
-                        print('izquierda')
-                        movimiento += 1
-
-                    if indice_fila + 1 <= 2:
-                        # si indice_fila fuese 2 (ultima columna) 2+1=3 no seria ni menor ni igual a 2 - no se podria
-                        # no se puede mover para arriba ni para la izquierda pero si para abajo
-
-                        # si le sumo uno  a la fila es menor o igual al numero de filas total
-                        tablero[indice_fila+1][indice_elemento], tablero[indice_fila][indice_elemento] = tablero[indice_fila][indice_elemento], tablero[indice_fila + 1][indice_elemento]
-                        print('abajo')
-                        movimiento += 1
-
-                    if indice_elemento + 1 <= 2:
-                        tablero[indice_fila][indice_elemento+1], tablero[indice_fila][indice_elemento] = tablero[indice_fila][indice_elemento], tablero[indice_fila][indice_elemento+1]
-                        print('derecha')
-                        movimiento += 1
-    
+        move(tablero)
+        movimiento += 1
     print(f'tablero desordenado en {movimiento} movimientos\n{tablero}')
     return tablero
 
 
+def busqueda_random(tablero_desordenado, tablero_inicial):
+    movimiento = 0
+    flag = True
+    while flag == True:
+        if tablero_desordenado == tablero_inicial:
+            print(f'eureka {tablero_desordenado} \n{tablero_inicial}')
+            print(f'encontre la solucion en {movimiento} movimientos')
+            flag = False
+        else:
+            move(tablero_desordenado)
 
 
 
 if __name__ == '__main__':
     tablero_inicial = [[1,2,3],[4,5,6],[7,8,'x']]
-    tablero_desordenado = desordenar(tablero_inicial)
+    tablero = copy.deepcopy(tablero_inicial)
+    tablero_desordenado = desordenar(tablero)
+
+    #busqueda_random(tablero_desordenado, tablero_inicial)  #no anda. 
+
+    print(tablero_inicial)
