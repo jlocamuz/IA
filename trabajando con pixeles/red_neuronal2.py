af3e2a9e552dce08164f072456cb641861e85808
jlocamuz <jlocamuz@gmail.com>
2022-11-29 13:02:35 -0300

    codigo optimizado

--- trabajando con pixeles/red_neuronal2.py
+++ trabajando con pixeles/red_neuronal2.py
@@ -4,56 +4,45 @@ import matplotlib.pyplot as plt
 from probando2 import *
 import random
 
-contador = 0
-
-
 def sigmoid(x):
-
 	sig = 1 / (1 + math.exp(-x))
 	return sig
 
-
 class Perceptron:
-	def __init__(self, capa, neurona, pesos, entradas=None, resultado=None, error=None):
+	def __init__(self, capa, neurona, nro_entradas, resultado=None, error=None):
 		self.capa = capa
 		self.neurona = neurona
-		self.pesos = pesos
-		self.resultado = resultado
+		self.pesos = [random.uniform(-0.01, 0.01) for _ in range(nro_entradas+1)]
+
 
 	def sumatoria(self, entrada):
 		# el cero siempre para el bias!
-
-		#print('len entrada', len(entrada))
 		z = 0  # inicializo en 0
 		for i in range(len(entrada)):
 			z += (entrada[i]) * self.pesos[i]
-
 		self.resultado = sigmoid(z)
-
-	# aca ta el errro
 
 	def actualizar_pesos(self, lr, entrada):
 		for i in range(len(entrada)):
 			delta = self.error * lr * entrada[i]
-			# print(delta)
-			#print("paso anterior", self.pesos[i])
 			self.pesos[i] += delta
-			#print("paso posterior", self.pesos[i])
 
 
 class RedNeuronal:
 	def __init__(self, x):
 		#print('creando Estructura Red Neuronal')
 		self.red = []
-		self.estructura = [i[0] for i in x]  # n capas ocultas
-		pesos = [i[1] for i in x]
+		self.estructura = x  # n capas ocultas
+				#[7680, 100, 1]
+		for capa, numero_neuronas in enumerate(self.estructura[1:]):
+			lista_neuronas = []
+			# i [100, 1] neurona [0-99]
+			for neurona in range(numero_neuronas):
+				# entradas? 
+				lista_neuronas.append(Perceptron(capa, neurona, self.estructura[capa]))
+			(self.red).append(lista_neuronas)
 
-		for index, capa in enumerate(self.estructura):
-			list_capa = []
-			for neurona in range(capa):
-				list_capa.append(Perceptron(
-					index, neurona, pesos[index][neurona]))
-			(self.red).append(list_capa)
+
 
 	def alimentarRed(self, data):
 		errorA = 1
@@ -65,8 +54,8 @@ class RedNeuronal:
 		epochs = 0
 		bias = 1
 		x1 = 0
-		while errorA > 0.005 and errorB > 0.005 and epochs < 40:
-			print(epochs)
+		while errorA > 0.001 and errorB > 0.001 :
+			print(f'epoch {epochs}')
 			for indice, dato in enumerate(data):
 				#print(f'indice {indice}')  IMPORTANTE
 				try:
@@ -76,28 +65,22 @@ class RedNeuronal:
 								# PRIMERA CAPA
 								# entran mis data.shape[:-1]  --> 1 x peso!  me queda un peso. para el bias :)
 								entrada = [bias] + list(dato[:-1])
-								print(len(entrada))
 
 							else:
 								# CAPAS INTERMEDIAS
-								resultados_anteriores = [
-									i.resultado for i in self.red[neurona.capa - 1]]
+								resultados_anteriores = [i.resultado for i in self.red[neurona.capa - 1]]
 								entrada = [bias] + resultados_anteriores
-
 							neurona.sumatoria(entrada)
-
 							neurona.error = dato[-1] - neurona.resultado
-							error = abs(neurona.error)
 							if indice % 2 == 0:
 								color = 'green'
 								errorA = abs(neurona.error)
-
 							else:
 								color = 'orange'
 								errorB = abs(neurona.error)
 
-							if indiceCapa == (len(self.estructura) - 1):
-								
+							# ultima capa. 
+							if len(capa) == 1:
 								plt.plot(x1, abs(neurona.error),"o", color=color)
 
 							# for i in neurona.pesos:
@@ -106,33 +89,18 @@ class RedNeuronal:
 							neurona.actualizar_pesos(lr, entrada)
 				except OverflowError:
 					print('pass')
-			# print(f'epoch #n {epochs}')
 			epochs += 1
 		plt.plot(x1, abs(errorA), label = "ERRORES Claudia",color='green')
 		plt.plot(x1, abs(errorB), label = "ERRORES Rosario", color='orange')
 		plt.legend()
 		plt.show()
 
-def pesos_random(entradas, neuronas):
-	pesos = [[random.uniform(-0.01, 0.01)
-			  for _ in range(entradas)] for _ in range(neuronas)]
-	return pesos
 
 
 if __name__ == '__main__':
 	personas = manipular_imagenes('./imagenes', './modificadas/')
-	# formato input [[x,y,z],[x,y,z]]
-	# mi formato input deberia ser [[pixel1, pixel2 , ..., pixel 7686, 0 (A) o 1 (B)]]
-
-	#     xor = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0]])
-	#red_neuronal = RedNeuronal([[3,[0.9, 0.7, 0.5],[0.3, -0.9, -1],[0.8, 0.35, 0.1]], [1, [-0.23, -0.79, 0.56, 0.6]]])
-
-	red_neuronal = RedNeuronal([
-			# pesos_random(entradas+1(bias), neuronas)
-		[100, pesos_random(7681, 100)],
-		[1, pesos_random(101, 1)]
-	])
-	#red_neuronal = RedNeuronal()
+	nro_entradas = len(personas[0]) - 1  # pq el ultimo elemento es 0-A y 1-B
+	lista = [nro_entradas, 100, 1]
+	red_neuronal = RedNeuronal(lista) # tomar primer parametro de lista como entradas.
+	
 	red_neuronal.alimentarRed(personas)
-
-
